f7b69fb6b6126ad6b1c564975938c5528c8a279d
shajib740 <shajib740@gmail.com>
2023-04-14 00:15:23 -0400

    Exception Handling

---
+++ AdvancedExceptionHandling/AdvancedExceptionHandling/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedExceptionHandling
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string ageText = "";
+            int ageNumber = 0;
+            try
+            {
+                ExceptionDemo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Custom Implementaion Exception");
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+
+
+                Console.Write("Enter your age: ");
+                ageText = Console.ReadLine();
+                ageNumber = ConvertToNumber(ageText);
+            
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex) when (ex.InnerException.InnerException.Message.ToLower() == "test" )
+            {
+                Console.WriteLine("When inner exception is null");
+                Console.WriteLine(ex.Message);
+            }
+            catch 
+            {
+                Console.WriteLine("Default exception");
+            }
+            finally { 
+
+                Console.WriteLine($"Age is {ageNumber}"); 
+            }
+            Console.ReadLine();
+        }
+
+        private static void ExceptionDemo()
+        {
+            throw new NotImplementedException("You forgot to implement this method");
+        }
+
+        private static int ConvertToNumber(string ageText)
+        {
+            bool isValidAge = int.TryParse(ageText, out int age);
+            if (isValidAge == false)
+            {
+                throw new InvalidCastException("Could not covert the age to number");
+            }
+            else if (age < 0)
+            {
+                throw new ArgumentOutOfRangeException("Invalid age", new ArgumentOutOfRangeException("Negative", new ArgumentException("Test")));
+            }
+            else
+            {
+                return age;
+            }
+        }
+    }
+}
---
+++ ExceptionHandlingDemo/ConsoleUI/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            //CheckingStackTrace();
+            try
+            {
+                BadCall();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an exception thrwon in our app");
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+            Console.ReadLine();
+        }
+
+        private static void CheckingStackTrace()
+        {
+            BadCall();
+        }
+
+            private static void BadCall()
+        {
+            int[] ages = new int[] { 1, 3, 5 };
+
+            for (int i = 0; i <= ages.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(ages[i]);
+                }
+                catch (Exception ex)
+                {
+                    // If you are writing to to console (Which means you are trying to show it in user interface) then catch
+                    // it at the very very top (layers, maybe this part is inside 3 layers of classes) because the inner
+                    // lasyer or libraries does not have access to the UI.
+                    // So throw it from here and display it at the top
+
+                 //   Console.WriteLine("We had an error");
+                   // Console.WriteLine(ex); // This is for log file as this gives line number of where it occured
+                   // Console.WriteLine(ex.Message); // This is for user
+                    //throw;                          
+                    throw new Exception("There was an error in our array", ex); // This will give the innerexception. Amd innerexception has the stacktrace also. So it will give the correct line number where error occured 
+                }
+            }
+        }
+    }
+}
