c0254c8cc49d4edfb46a50ffe158007481b36210
shajib740 <shajib740@gmail.com>
2023-06-15 22:28:39 -0400

    Unit Test Project Type Added

---
+++ CalculatorUnitTest/CalculatorLibrary.Tests/CalculationTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary.Tests
+{
+    public class CalculationTests
+    {
+        [Theory]
+        [InlineData(4,6,10)]
+        [InlineData(0,0,0)]
+        [InlineData(-4,-6,-10)]
+        [InlineData(4,-6,-2)]
+        public void AddShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculator calc = new Calculator();
+            //Act
+            double actual = calc.Add(x, y);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(8,2,6)]
+        [InlineData(0,0,0)]
+        [InlineData(-5,-5,0)]
+        public void SubtractShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculator calc = new Calculator();
+            //Act
+            double actual = calc.Subtract(x, y);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(0,0,0)]
+        [InlineData(40,0,0)]
+        [InlineData(-10,3,-30)]
+        [InlineData(-10,-3,30)]
+        public void MultiplyShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculator calc = new Calculator();
+            //Act
+            double actual = calc.Multiply(x, y);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(0,1,0)]
+        [InlineData(5,1,5)]
+        [InlineData(0, -5, 0)]
+        [InlineData(0, 0, 1)]
+
+        public void DivideShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculator calc = new Calculator();
+            //Act
+            double actual = calc.Divide(x, y);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CalculatorUnitTest/CalculatorLibrary/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            double output = x + y;
+            return output;
+        }
+        public double Subtract(double x, double y)
+        {
+            double output = x - y;
+            return output;
+        }
+        public double Multiply(double x, double y)
+        {
+            double output = x * y;
+            return output;
+        }
+        public double Divide(double x, double y)
+        {
+            double output;
+            if (x == 0)
+            {
+                output = 0;
+            }
+            else
+            {
+                output = (x / y);
+            }
+            return output;
+        }
+    }
+}
